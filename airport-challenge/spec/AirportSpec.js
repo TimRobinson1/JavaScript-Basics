a2a26b98ad253f34d8a326814bb1b4fcdc93e2cb
Timothy Robinson <timmyrobby@gmail.com>
2017-05-09 15:45:03 +0100

    Starts Airport Challenge

---
+++ airport-challenge/spec/AirportSpec.js
@@ -0,0 +1,9 @@
+describe("Airport", function() {
+  beforeEach(function() {
+    airport = new Airport();
+  });
+
+  it("exists as an object", function() {
+    expect(airport).toBeDefined();
+  });
+});
