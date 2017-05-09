c5c0d627aa830af5cea42b8b15b173548b6c7a19
Timothy Robinson <timmyrobby@gmail.com>
2017-05-09 15:51:12 +0100

    Airport can signal planes to land

--- airport-challenge/spec/AirportSpec.js
+++ airport-challenge/spec/AirportSpec.js
@@ -1,9 +1,19 @@
 describe("Airport", function() {
   beforeEach(function() {
     airport = new Airport();
+    plane = 'placeholder_plane'
   });
 
   it("exists as an object", function() {
     expect(airport).toBeDefined();
+  });
+
+  it("starts with no planes docked", function() {
+    expect(airport.hangar).toEqual([]);
+  });
+
+  it("adds planes to hangar when landing them", function() {
+    airport.land(plane);
+    expect(airport.hangar).toEqual([plane]);
   });
 });
