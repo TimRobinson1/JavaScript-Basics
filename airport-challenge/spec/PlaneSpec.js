497147e76a41f5c74fde3cb74e2d0ba6b2a02fd4
Timothy Robinson <timmyrobby@gmail.com>
2017-05-09 16:11:10 +0100

    Adds a isFlying status for plane object

--- airport-challenge/spec/PlaneSpec.js
+++ airport-challenge/spec/PlaneSpec.js
@@ -1,13 +1,26 @@
 describe("Plane", function() {
   beforeEach(function() {
     plane = new Plane();
+    airport = new Airport();
   });
+
 
   it("exists as an object", function() {
     expect(plane).toBeDefined();
   });
 
   it("starts as flying", function() {
+    expect(plane.isFlying).toEqual(true);
+  });
+
+  it("can be given a landed status", function() {
+    plane.land();
+    expect(plane.isFlying).toEqual(false)
+  });
+
+  it("can be set to flying after landing", function() {
+    plane.land();
+    plane.takeoff();
     expect(plane.isFlying).toEqual(true);
   });
 });
--- airport-challenge/src/Plane.js
+++ airport-challenge/src/Plane.js
@@ -1,3 +1,11 @@
 function Plane() {
   this.isFlying = true;
 };
+
+Plane.prototype.land = function() {
+  this.isFlying = false;
+};
+
+Plane.prototype.takeoff = function() {
+  this.isFlying = true;
+};
